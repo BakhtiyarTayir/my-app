8a2705124c4619186208d1f1bf95d465a8e2cff8
bakhtiyarTayir <bakhtiyarTayir@gmail.com>
2023-05-06 17:59:49 +0500

    bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,17 +5,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Document</title>
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="{{ route('main') }}">Main</a></li>
-            <li><a href="{{ route('about') }}">About</a></li>
-            <li><a href="{{ route('post') }}">Post</a></li>
-            <li><a href="{{ route('contact') }}">Contacts</a></li>
+    <nav class="container">
+        <ul class="list-group">
+            <li class="list-group-item"><a href="{{ route('main') }}">Main</a></li>
+            <li class="list-group-item"><a href="{{ route('about') }}">About</a></li>
+            <li class="list-group-item"><a href="{{ route('post') }}">Post</a></li>
+            <li class="list-group-item"><a href="{{ route('contact') }}">Contacts</a></li>
         </ul>
     </nav>
+
     @yield('content')
 </body>
 </html>
